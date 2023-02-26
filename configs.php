8c94a72f8d26fb849d0b9ae85cd11e3276525c79
Шамиль Хайрулаев <sa.hairulaev@gmail.com>
2023-02-27 01:25:52 +0300

    Changed AmoData

--- configs.php
+++ configs.php
@@ -9,8 +9,8 @@ error_reporting(E_ALL);
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/functions.php';
 
-const CLIENT_ID = '1a3557cf-8a39-4cae-b656-6b5c5db90855';
-const CLIENT_SECRET = '6BKpVw0oH3xVNbXERyyS0YBf24nA8Au694TmxXTgPZrhgHigKUmi7BBkUgCTmbo3';
+const CLIENT_ID = 'dd92b0bb-4ab4-4d6c-bea0-24e6a6745e3c';
+const CLIENT_SECRET = 'bHFqROqced2rA9gDDyxlXrEVTohR17c8uAop99c1sfBnOTcZEXGqGOUsgBrUuTss';
 const REDIRECT_URI = 'https://webhook.site/84f5ff9b-3b2c-4815-a63f-600749438833';
 const SUBDOMAIN = 'emfytestexc.kommo.com';
 
